36721fcc62e7011820928ca30f6fed38aeb87c4f
Shukhrat Nasriddinov <shukhratnasriddinov1984@gmail.com>
2024-07-21 16:15:57 -0400

    last upgrade

---
+++ js/blogs.js
@@ -0,0 +1,46 @@
+//alert("Hello world");
+
+
+
+//-------------------------------------------------------------------
+
+//-------------------------------------------------------------------
+
+
+//---------------------------------container-header---------------------------------
+$(".navmenu_2 a").on("click", function() {
+    let position = $(this)
+        .parent()
+        .position();
+    let width = $(this)
+        .parent()
+        .width();
+    $(".navmenu_2 .slide1").css({ opacity: 1, left: +position.left, width: width });
+});
+$(".navmenu_2 a").on("mouseover", function() {
+    let position = $(this)
+        .parent()
+        .position();
+    let width = $(this)
+        .parent()
+        .width();
+    $(".navmenu_2 .slide2")
+        .css({
+            opacity: 1,
+            left: +position.left,
+            width: width
+        })
+        .addClass("squeeze");
+});
+$(".navmenu_2 a").on("mouseout", function() {
+    $(".navmenu_2 .slide2")
+        .css({ opacity: 0 })
+        .removeClass("squeeze");
+});
+let currentWidth = $(".navmenu_2")
+    .find(".active")
+    .parent("li")
+    .width();
+let current = $(".navmenu_2 .active").position();
+$(".navmenu_2 .slide1").css({ left: +current.left, width: currentWidth });
+//---------------------------------container-header---------------------------------
